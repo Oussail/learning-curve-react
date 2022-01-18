1c446e9a153d3a364931d03de79f16d60eacd86d
Oussail <oussail.alaoui@gmail.com>
2022-01-18 21:53:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { id: 'dsqds', name:"Oussail", age :25 },
+      { id: 'fsdfsd', name:"Stephanie", age :20 }
+    ],
+    showPersons : false
+  }
+  
+  togglePersonsHandler =  () => {
+    this.setState({
+      showPersons: !this.state.showPersons
+    })
+  }
+
+  deletePersonHandler =  (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({
+      persons: persons
+    });
+  }
+
+  nameChangedHandler = (event, personId) => {
+    const persons = [...this.state.persons];
+    const p = persons.find(p => p.id === personId);
+    p.name = event.target.value;
+    this.setState({
+      persons: persons
+    });
+  }
+
+
+  
+
+  render() {
+    const style = {
+      backgroundColor: 'green',
+      color:'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding : '8px',
+      cursor: 'pointer',
+    };
+    let persons = null;
+    if(this.state.showPersons) {
+
+      persons = this.state.persons.map((person, index) => (
+        <Person changed={(event) => {this.nameChangedHandler(event, person.id)}} click={() => {this.deletePersonHandler(index)}} name={person.name} age={person.age} key={person.id}/>
+      ));
+
+      style.backgroundColor = 'red';
+    }
+    return (
+      <div className="App">
+        <h1>Test</h1>
+        <p>This is really working</p>
+        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
+      </div>
+    ) 
+  }
 }
 
 export default App;
